4d501d4386ac9e7bda9f9a508cd1a9a726ebb8ac
tamas <hrabotam@gmail.com>
2019-06-09 19:39:12 +0300

    heroku initial commit

---
+++ build/precache-manifest.60baedde1861ba82eeeec012ae970d4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10321b4ac829982df52bec151be72cf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bae44bd3ea0db283148a",
+    "url": "/static/css/main.360eedb3.chunk.css"
+  },
+  {
+    "revision": "0f44f52868a50c2f6362",
+    "url": "/static/js/2.d23e1248.chunk.js"
+  },
+  {
+    "revision": "bae44bd3ea0db283148a",
+    "url": "/static/js/main.f1ccdd21.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
